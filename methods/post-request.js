13023a96d24a1dc4cb7be61ec12c31eb97eca233
DAVRUNKS <DAVRUNKSVIRGIFLIPS@GITHUB.COM>
2024-01-05 01:54:33 +0300

    first commit

---
+++ methods/post-request.js
@@ -0,0 +1,61 @@
+const crypto = require("crypto");
+const requestBodyparser = require("../util/body-parser");
+const writeToFile = require("../util/write-to-file");
+
+module.exports = async (req, res) => {
+  if (req.url === "/api/movies") {
+    try {
+      let body = await requestBodyparser(req);
+      console.log("request body: ", body);
+      body.id = crypto.randomUUID();
+      req.movies.push(body);
+      writeToFile(req.movies);
+      // Set headers before sending the response
+      res.writeHead(201, { "Content-Type": "application/json" });
+      res.end();
+    } catch (err) {
+      console.log(err);
+      // Set headers before sending the response
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          title: "Validation Failed",
+          message: "Request body is not valid",
+        })
+      );
+    }
+  } else {
+    // Set headers before sending the response
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ title: "Not Found", message: "Route not found" }));
+  }
+};
+
+// const crypto = require("crypto");
+// const requestBodyparser = require("../util/body-parser");
+// const writeToFile = require("../util/write-to-file");
+// module.exports = async (req, res) => {
+//   if (req.url === "/api/movies") {
+//     try {
+//       let body = await requestBodyparser(req);
+//       console.log("request body: ", body);
+//       body.id = crypto.randomUUID();
+//       req.movies.push(body);
+//       writeToFile(req.movies);
+//       res.setHeader(201, { "Content-Type": "application/json" });
+//       res.end();
+//     } catch (err) {
+//       console.log(err);
+//       res.setHeader(400, { "Content-Type": "application/json" });
+//       res.end(
+//         JSON.stringify({
+//           title: "Validation Failed",
+//           message: "Request body is not valid",
+//         })
+//       );
+//     }
+//   } else {
+//     res.setHeader(404, { "Content-Type": "application/json" });
+//     res.end(JSON.stringify({ title: "Not Found", message: "Route not found" }));
+//   }
+// };
---
+++ server.js
@@ -0,0 +1,69 @@
+const http = require("http");
+const deleteReq = require("./methods/delete-request");
+const putReq = require("./methods/put-request");
+const postReq = require("./methods/post-request");
+const getReq = require("./methods/get-request");
+let movies = require("./data/movies.json");
+
+const PORT = process.env.PORT || 5001;
+
+const server = http.createServer((req, res) => {
+  req.movies = movies;
+
+  switch (req.method) {
+    case "GET":
+      getReq(req, res);
+      break;
+    case "POST":
+      postReq(req, res);
+      break;
+    case "PUT":
+      putReq(req, res);
+      break;
+    case "DELETE":
+      deleteReq(req, res);
+      break;
+    default:
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Route not found" }));
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
+});
+// const http = require("http");
+// //require("dotenv").config();
+// const deleteReq = require("./methods/delete-request");
+// const putReq = require("./methods/put-request");
+// const postReq = require("./methods/post-request");
+// const getReq = require("./methods/get-request");
+// let movies = require("./data/movies.json");
+
+// const PORT = process.env.PORT || 5001;
+
+// const server = http.createServer((req, res) => {
+//   req.movies = movies;
+
+//   switch (req.method) {
+//     case "GET":
+//       getReq(req, res);
+//       break;
+//     case "POST":
+//       postReq(req, res);
+//       break;
+//     case "PUT":
+//       putReq(req, res);
+//       break;
+//     case "DELETE":
+//       deleteReq(req, res);
+//       break;
+//   }
+//   res.statusCode = 404;
+//   res.setHeader("Content-Type", "applcation/json");
+//   res.write(JSON.stringify({ message: "Route not found" }));
+//   res.end();
+// });
+// server.listen(PORT, () => {
+//   console.log(`Server started on port : ${PORT}`);
+// });
