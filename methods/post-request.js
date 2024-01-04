6620b36a30f01fbc5bf034c27d16adff1b68bb7c
DAVRUNKS <DAVRUNKSVIRGIFLIPS@GITHUB.COM>
2024-01-05 02:01:16 +0300

    new lines added

--- methods/post-request.js
+++ methods/post-request.js
@@ -30,32 +30,3 @@ module.exports = async (req, res) => {
     res.end(JSON.stringify({ title: "Not Found", message: "Route not found" }));
   }
 };
-
-// const crypto = require("crypto");
-// const requestBodyparser = require("../util/body-parser");
-// const writeToFile = require("../util/write-to-file");
-// module.exports = async (req, res) => {
-//   if (req.url === "/api/movies") {
-//     try {
-//       let body = await requestBodyparser(req);
-//       console.log("request body: ", body);
-//       body.id = crypto.randomUUID();
-//       req.movies.push(body);
-//       writeToFile(req.movies);
-//       res.setHeader(201, { "Content-Type": "application/json" });
-//       res.end();
-//     } catch (err) {
-//       console.log(err);
-//       res.setHeader(400, { "Content-Type": "application/json" });
-//       res.end(
-//         JSON.stringify({
-//           title: "Validation Failed",
-//           message: "Request body is not valid",
-//         })
-//       );
-//     }
-//   } else {
-//     res.setHeader(404, { "Content-Type": "application/json" });
-//     res.end(JSON.stringify({ title: "Not Found", message: "Route not found" }));
-//   }
-// };
